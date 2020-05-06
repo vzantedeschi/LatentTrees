2d7f4ca0b28cf06c2e10a1aabfdb687dcda3a8c2
Vlad Niculae <vlad@vene.ro>
2020-05-06 15:41:44 +0100

    so close and yet so far

--- scripts/check_treeqp.py
+++ scripts/check_treeqp.py
@@ -29,10 +29,10 @@ def solve2(eta, box=False):
 
     prob = cx.Problem(cx.Minimize(obj), constr)
     prob.solve()
-    return d.value.round(3)
+    return d.value
 
 
-# not fully worked out
+# this is wrong, but the deep one handles this case correctly
 def closed_form(eta):
 
     # tentative solution: d=-eta
@@ -74,15 +74,133 @@ def main():
     print(np.clip(d, a_min=0, a_max=1))
 
 
+def print_as_tree(d):
+    print(d[0])
+    print(d[1], "\t\t\t", d[2])
+    print(d[3], d[4], d[5], d[6])
+
+
+def deep_closed_form(eta, verbose=False):
+    coloring = np.arange(len(eta))
+    parent = [None, 0, 0, 1, 1, 2, 2]
+    sibling = [None, 2, 1, 4, 3, 6, 5]
+    bottom_up = [3, 4, 5, 6, 1, 2, 0]
+
+    d = -eta
+
+
+# version that finalizes both children before tackling parent
+# is incorrect
+#
+#    for i in bottom_up:
+#         # update self and children, downward
+#         val = np.mean(-eta[coloring == i])
+#         d[coloring == i] = val
+#         if verbose:
+#             print(i, d)
+#
+#         # check for violation upward
+#         p = parent[i]
+#         if p is not None and d[i] > d[p]:
+#             coloring[coloring == i] = p
+#             if verbose:
+#                 print('joining', i, 'into', p)
+
+    children = [[1, 2], [3, 4], [5, 6], [], [], [], []]
+
+    # greedy bottom-up, largest sibling first.
+    for i in bottom_up:
+
+        # swap with sibling
+        if sibling[i] is not None and d[sibling[i]] > d[i]:
+            i = sibling[i]
+            bottom_up[bottom_up.index(sibling[i])] = i
+
+        p = parent[i]
+        if p is None:
+            continue
+        if d[i] > d[p]:
+            coloring[coloring == i] = p
+            val = np.mean(-eta[coloring == p])
+            d[coloring == p] = val
+            if verbose:
+                print('joining', i, 'into', p, d)
+
+    # top down propagate any possible created violations
+    queue = [0]
+    while len(queue):
+        pp = queue.pop()
+        mychildren = children[pp]
+
+        for ii in mychildren:
+            queue.append(ii)
+            color = None
+            if d[ii] > d[pp]:
+                color = coloring[pp]
+                coloring[coloring == ii] = color
+                val = np.mean(-eta[coloring == color])
+                d[coloring == color] = val
+                if verbose:
+                    print('td-join', pp, 'into', ii, d)
+
+
+    return d
+
+
 def wip():
-    eta = np.array([0, -1, 0, .1, -.1, 0, -2], dtype=np.double)
-    print(solve2(eta, box=True))
+    eta = np.array([0, -1, 0, .1, -.1, 0, -3], dtype=np.double)
+    print(solve2(eta, box=False))
+    deep_closed_form(eta, verbose=True)
+
+    print()
+    print()
 
     eta = np.array([0, -1, 0, .1, -.1, 0, -0.5], dtype=np.double)
-    print(solve2(eta, box=True))
+    print(solve2(eta, box=False))
+    deep_closed_form(eta, verbose=True)
+
+    print()
+    print()
+
+    rng = np.random.RandomState(42)
+    for _ in range(100):
+        eta = rng.randn(7)
+        eta /= np.sqrt(np.sum(eta ** 2))
+
+        d_expected = solve2(eta, box=False)
+        d_obtained = deep_closed_form(eta, verbose=False)
+
+        if not np.allclose(d_expected, d_obtained):
+            print()
+            print(-eta)
+            d_obtained = deep_closed_form(eta, verbose=True)
+            print(d_expected, np.sum((d_expected + eta) ** 2))
+            print(d_obtained, np.sum((d_obtained + eta) ** 2))
+            # print_as_tree(d_obtained)
+            print()
+
+
+def what():
+    eta = np.array([0, 1, .25])
+
+    for t in [.1, .2, .3, .4,  .5, .6, .7]:
+        eta[1] = t
+        print(solve(-eta, box=False), "(true)")
+        # print(closed_form(-eta), "(incorrect)")
+        eta2 = np.full(7, -99.0)
+        eta2[:3] = eta
+        print(deep_closed_form(-eta2, verbose=False)[:3])
+
+    # eta = np.array([-.517, -.091, .303])
+    # print(solve(-eta, box=False), "(true)")
+    # eta2 = np.full(7, -99.0)
+    # eta2[:3] = eta
+    # print(deep_closed_form(-eta2, verbose=True))
 
 
 if __name__ == '__main__':
-    main()
-    # wip()
+    np.set_printoptions(suppress=True, precision=3)
+    # main()
+    wip()
+    # what()
 
