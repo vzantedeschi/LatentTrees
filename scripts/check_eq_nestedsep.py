21a8873f6012e5893fad36d896fc20743ffffcf6
Vlad Niculae <vlad@vene.ro>
2020-05-13 15:36:52 +0100

    solve inner optim

--- scripts/check_eq_nestedsep.py
+++ scripts/check_eq_nestedsep.py
@@ -1,9 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def solve(eta, qs):
+    qs_gtr = qs[qs >= eta]
+    ix = np.argsort(qs_gtr)[::-1]
+    qs_srt = qs_gtr[ix]
+
+    d = eta
+    for k in range(len(qs_srt)):
+        if d > qs_srt[k]:
+            break
+        topk = qs_srt[:k + 1]
+        d = (eta + np.sum(topk)) / (k + 2)
+    return d
+
+
 def main():
     eta = .5
-    # qs = np.array([.1])
 
     ds = np.linspace(-1, 2, 5000)
     def curve(qs):
@@ -12,7 +25,9 @@ def main():
             val += np.sum((d - qs[qs >= d]) ** 2)
             return val / 2
         curve = [f(d) for d in ds]
-        print(ds[np.argmin(curve)])
+        print("numeric min", ds[np.argmin(curve)])
+        print("analytic min", solve(eta, qs))
+        print()
         return curve
 
     qs = np.array([.4])
@@ -34,6 +49,9 @@ def main():
     plt.plot(ds, curve(qs), label=str(qs))
 
     qs = np.array([2.0, 1.5, 1.2, 1])
+    plt.plot(ds, curve(qs), label=str(qs))
+
+    qs = np.array([2.0, 2.0])
     plt.plot(ds, curve(qs), label=str(qs))
 
     plt.plot(ds, 0.5 * (ds - eta) ** 2, ls=":")
