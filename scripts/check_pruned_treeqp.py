6f793eca182625d5c886e062dcce7fc61e3621e7
Valentina Zantedeschi <valentina.zantedeschi@univ-st-etienne.fr>
2020-05-14 13:32:48 +0200

    loop on step 2 (min d over nodes of same color)

--- scripts/check_pruned_treeqp.py
+++ scripts/check_pruned_treeqp.py
@@ -2,32 +2,57 @@ import numpy as np
 import cvxpy as cx
 
 def closed_form_1D(eta, qs):
+
     qs_gtr = qs[qs >= eta]
     ix = np.argsort(qs_gtr)[::-1]
     qs_srt = qs_gtr[ix]
 
     d = eta
+    
+    k = 0
     for k in range(len(qs_srt)):
         if d > qs_srt[k]:
+            k += 1
             break
-        topk = qs_srt[:k + 1]
-        d = (eta + np.sum(topk)) / (k + 2)
+
+    topk = qs_srt[:k]
+    d = (eta + np.sum(topk)) / (k + 1)
+    
     return d
+
+def closed_form_colored(d, eta, qs):
+
+    topk = 0
+    nb_k = 0
+    for d_t, q_t in zip():
+
+        qs_gtr = q_t[q_t >= d_t]
+        ix = np.argsort(qs_gtr)[::-1]
+        qs_srt = qs_gtr[ix]
+        
+        for k in range(len(qs_srt)):
+            if d_t > qs_srt[k]:
+                break
+                
+            topk += qs_srt[k]
+            nb_k += 1
+
+    new_d = (np.sum(eta) + np.sum(topk)) / (len(eta) + nb_k)
+    
+    return new_d
 
 def closed_form(eta, qs, verbose=False):
 
-    d = eta
+    d = eta.copy()
     n = len(d)
+
+    parent = [None, 0, 0, 1, 1, 2, 2]
+    coloring = np.arange(n)
 
     for t, e in enumerate(eta):
 
         d[t] = closed_form_1D(e, qs[:, t]) 
 
-    # todo: generalize parent vector by depth. Rest of code is ok
-    # IIRC from college this can be done with some modulo arithmetic.
-    parent = [None, 0, 0, 1, 1, 2, 2]
-    coloring = np.arange(n)
-
     while True:
         max_violating_d = -np.inf
         max_violating_ix = None
@@ -48,15 +73,17 @@ def closed_form(eta, qs, verbose=False):
         p = parent[t]
         pc = coloring[p]
         coloring[coloring == t] = pc
-        d[coloring == pc] = np.mean(eta[coloring == pc])
+
+        pc_ix = (coloring == pc)
+        d[pc_ix] = closed_form_colored(d[pc_ix], eta[pc_ix], qs[:, pc_ix])
         if verbose:
             print("joining", t, p, d)
 
-    return d
+    return np.clip(d, 0, 1)
 
 def noq_closed_form(eta, verbose=False):
 
-    d = eta
+    d = eta.copy()
     n = len(d)
 
     # todo: generalize parent vector by depth. Rest of code is ok
@@ -88,7 +115,7 @@ def noq_closed_form(eta, verbose=False):
         if verbose:
             print("joining", t, p, d)
 
-    return d
+    return np.clip(d, 0, 1)
 
 # def solve_qp(eta, qs, box=False):
 #     parent = [None, 0, 0, 1, 1, 2, 2]
@@ -97,7 +124,9 @@ def noq_closed_form(eta, verbose=False):
 #     d = cx.Variable(eta.shape)
 
 #     obj = .5 * cx.sum_squares(d - eta)
-#     obj += .5 * cx.sum([cx.sum_squares(d - qs[i, qs[i] >= d]) for i in range(N)])
+
+#     violations = cx.transforms.indicator([qs[i] >= d for i in range(N)])
+#     obj += .5 * cx.sum_squares([(d - qs[i])**2 for i in range(N)])
 
 #     constr = [d[i] <= d[parent[i]] for i in range(1, T)]
 
@@ -115,7 +144,7 @@ def main():
     np.random.seed(SEED)
 
     eta = np.array([1, -1, 0, 1, 3, -0.1, 0.1], dtype=np.double)
-    qs = np.random.uniform(size = (1, 7))
+    qs = np.random.uniform(size = (2, 7))
 
     print("eta", eta)
     print("qs", qs)
@@ -123,26 +152,27 @@ def main():
     print()
 
     closed_form(eta, qs, verbose=True)
+    # solve_qp(eta, qs)
 
     print("\ncheck with no qs")
     qs = np.zeros((1, 7))
     print(closed_form(eta, qs, verbose=False))
     print(noq_closed_form(eta))
 
-    for _ in range(1000):
-        eta = np.random.uniform(size = (7))
+    # for _ in range(10000):
+    #     eta = np.random.uniform(-3, 3, size = (7))
 
-        d_expected = noq_closed_form(eta)
-        d_obtained = closed_form(eta, qs)
+    #     d_expected = noq_closed_form(eta)
+    #     d_obtained = closed_form(eta, qs)
 
-        if not np.allclose(d_expected, d_obtained):
-            print()
-            print(-eta)
-            d_obtained = deep_closed_form(eta, verbose=True)
-            print(d_expected, np.sum((d_expected + eta) ** 2))
-            print(d_obtained, np.sum((d_obtained + eta) ** 2))
-            # print_as_tree(d_obtained)
-            print()
+    #     if not np.allclose(d_expected, d_obtained):
+    #         print()
+    #         print("eta", eta)
+    #         d_obtained = closed_form(eta, qs, verbose=True)
+    #         print(d_expected, np.sum((d_expected - eta) ** 2))
+    #         print(d_obtained, np.sum((d_obtained - eta) ** 2))
+    #         # print_as_tree(d_obtained)
+    #         print()
 
 
 if __name__ == '__main__':
