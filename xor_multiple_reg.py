fcf8d70a3ba6ecb575cd34dfc921b70a9e21a87b
Valentina Zantedeschi <valentina.zantedeschi@univ-st-etienne.fr>
2020-05-29 14:43:04 +0000

    minors

--- xor_multiple_reg.py
+++ xor_multiple_reg.py
@@ -1,12 +1,8 @@
 import numpy as np
-
-import matplotlib.pyplot as plt
-from matplotlib.colors import LinearSegmentedColormap
+import torch
 
 from src.datasets import toy_dataset
 from src.optimization import train_batch
-# from src.trees import str_as_bst
-from src.utils import make_directory
 
 DISTR = "xor"
 N = 1000
@@ -24,5 +20,5 @@ X, Y = toy_dataset(N, DISTR)
 for reg in [10**i for i in range(-2, 2)]:
     for norm in [0, 1, float('inf')]:
 
-    # train latent class tree and logistic regressor
-    model = train_batch(X, Y, bst_depth=TREE_DEPTH, nb_iter=ITER, lr=LR, reg=reg, norm=norm)
+        # train latent class tree and logistic regressor
+        model = train_batch(X, Y, bst_depth=TREE_DEPTH, nb_iter=ITER, lr=LR, reg=reg, norm=norm)
