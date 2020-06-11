d6a33edfde349f37977e1b70dad0c6d0930ac523
Vlad Niculae <vlad@vene.ro>
2020-06-11 15:49:01 +0100

    add test

--- src/qp.py
+++ src/qp.py
@@ -52,6 +52,64 @@ def pruning_qp(q, eta):
     return PruningQPFast.apply(q, eta)
 
 
+def _pruning_qp_subproblem(q, eta, idx):
+    topk = 0
+    nb_k = 0
+
+    d = torch.mean(eta[idx])
+
+    # select qs greater than current d (violating the constraints)
+    q_sorted, _ = torch.sort(q[:, idx][q[:, idx] >= d], descending=True)
+
+    for k in range(len(q_sorted)):
+        if d > q_sorted[k]:
+            break
+        topk += q_sorted[k]
+        nb_k += 1
+
+        d = (torch.sum(eta[idx]) + topk) / (len(eta[idx]) + nb_k)
+
+    return d
+
+
+def pruning_qp_slow(q, eta, bst):
+    # init d and colors different for all nodes
+    d = eta.clone()
+    coloring = np.arange(bst.nb_nodes)
+
+    for c in coloring:
+        d[c] = _pruning_qp_subproblem(q, eta, [c])
+
+    n_iter = 0
+
+    while True:
+        n_iter += 1
+        max_violating_d = -np.inf
+        max_violating_ix = None
+
+        for t in range(1, bst.nb_nodes):
+            # if edge is violating, and is larger than max so far
+            p = bst.parent(t)
+            if d[t] > d[p] and d[t] > max_violating_d:
+                max_violating_d = d[t]
+                max_violating_ix = t
+
+        if max_violating_ix is None:
+            # no more violations, we are done
+            break
+
+        # fix the selected violating edge, propagating along color.
+        # invariant: always keep the color of the parent.
+        p = bst.parent(max_violating_ix)
+        pc = coloring[p]
+        coloring[coloring == max_violating_ix] = pc
+
+        pc_ix = (coloring == pc)
+        d[pc_ix] = _pruning_qp_subproblem(q, eta, pc_ix)
+
+    return d
+
+
 class LatentDT(torch.nn.Module):
 
     def __init__(self, bst_depth, dim, pruned=True):
@@ -71,34 +129,8 @@ class LatentDT(torch.nn.Module):
 
         if self.pruned:
 
-            self.d = d_slow = self._compute_d_slow(q)
-            print("d slow", self.d)
-            print("grad", torch.autograd.grad(d_slow[0], q,
-                retain_graph=True))
-            print("grad", torch.autograd.grad(d_slow[1], q,
-                retain_graph=True))
-            # print("grad", torch.autograd.grad(d_slow[0], self.eta,
-                # retain_graph=True))
-            # print("grad", torch.autograd.grad(d_slow[1], self.eta,
-                # retain_graph=True))
-            # print("grad", torch.autograd.grad(d_slow[3], self.eta,
-                # retain_graph=True))
-            self.d = d_fast = pruning_qp(q, self.eta)
-            print("d fast", self.d)
-            print("grad", torch.autograd.grad(d_fast[0], q,
-                retain_graph=True))
-            print("grad", torch.autograd.grad(d_fast[1], q,
-                retain_graph=True))
-            # print("grad", torch.autograd.grad(d_fast[0], self.eta,
-                # retain_graph=True))
-            # print("grad", torch.autograd.grad(d_fast[1], self.eta,
-                # retain_graph=True))
-            # print("grad", torch.autograd.grad(d_fast[3], self.eta,
-                # retain_graph=True))
-            exit()
-            print(torch.norm(d_slow - d_fast).item())
-            exit()
-
+            self.d = pruning_qp(q, self.eta)
+            self.d = torch.clamp(self.d, 0, 1)
             z = torch.clamp(q, 0, 1)
             z = torch.min(z, self.d)
 
@@ -108,73 +140,8 @@ class LatentDT(torch.nn.Module):
         return z
 
     def predict(self, x):
-
         z = self.forward(x).detach().numpy()
-
         return self.bst.predict(z)
-
-    def _compute_d_slow(self, q):
-
-        # init d and colors different for all nodes
-        d = self.eta.clone()
-        coloring = np.arange(self.bst.nb_nodes)
-
-        for c in coloring:
-            d[c] = self._compute_d_colored_slow(q, [c])
-
-        n_iter = 0
-
-        while True:
-            n_iter += 1
-            max_violating_d = - np.inf
-            max_violating_ix = None
-
-            for t in range(1, self.bst.nb_nodes):
-                # if edge is violating, and is larger than max so far
-                p = self.bst.parent(t)
-                if d[t] > d[p] and d[t] > max_violating_d:
-                    max_violating_d = d[t]
-                    max_violating_ix = t
-
-            if max_violating_ix is None:
-                # no more violations, we are done
-                break
-
-            # fix the selected violating edge, propagating along color.
-            # invariant: always keep the color of the parent.
-            p = self.bst.parent(max_violating_ix)
-            pc = coloring[p]
-            coloring[coloring == max_violating_ix] = pc
-            # print("join ", max_violating_ix, "into", p)
-            # print(coloring)
-
-            pc_ix = (coloring == pc)
-            d[pc_ix] = self._compute_d_colored_slow(q, pc_ix)
-
-        d = torch.clamp(d, 0, 1)
-        print("Took", n_iter, "iter")
-
-        return d
-
-    def _compute_d_colored_slow(self, q, idx):
-
-        topk = 0
-        nb_k = 0
-
-        d = torch.mean(self.eta[idx])
-
-        # select qs greater than current d (violating the constraints)
-        q_sorted, _ = torch.sort(q[:, idx][q[:, idx] >= d], descending=True)
-
-        for k in range(len(q_sorted)):
-            if d > q_sorted[k]:
-                break
-            topk += q_sorted[k]
-            nb_k += 1
-
-            d = (torch.sum(self.eta[idx]) + topk) / (len(self.eta[idx]) + nb_k)
-
-        return d
 
     def _compute_q(self, x):
 
@@ -185,14 +152,19 @@ class LatentDT(torch.nn.Module):
 
         # upper bound children's q to parent's q
         # trick to avoid inplace operations involving A
-        q[:, self.bst.desc_left] = torch.min(q[:, self.bst.split_nodes], XA[:, self.bst.split_nodes])
-        q[:, self.bst.desc_right] = torch.min(q[:, self.bst.split_nodes], -XA[:, self.bst.split_nodes])
+        q[:, self.bst.desc_left] = torch.min(q[:, self.bst.split_nodes],
+                                             XA[:, self.bst.split_nodes])
+        q[:, self.bst.desc_right] = torch.min(q[:, self.bst.split_nodes],
+                                              -XA[:, self.bst.split_nodes])
 
         for _ in range(self.bst.depth):
-            q[:, self.bst.desc_left] = torch.min(q[:, self.bst.desc_left], q[:, self.bst.split_nodes])
-            q[:, self.bst.desc_right] = torch.min(q[:, self.bst.desc_right], q[:, self.bst.split_nodes])
+            q[:, self.bst.desc_left] = torch.min(q[:, self.bst.desc_left],
+                                                 q[:, self.bst.split_nodes])
+            q[:, self.bst.desc_right] = torch.min(q[:, self.bst.desc_right],
+                                                  q[:, self.bst.split_nodes])
 
         return q
+
 
 def main():
     # torch.manual_seed(42)
@@ -203,6 +175,7 @@ def main():
 
     dt = LatentDT(bst_depth=3, dim=dim)
     Z = dt(X)
+    print(Z)
 
 
 if __name__ == '__main__':
