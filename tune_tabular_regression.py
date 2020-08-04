a113a5e457d6d3f62dab2582cbcabd822a22e8d8
Valentina Zantedeschi <valentina.zantedeschi@univ-st-etienne.fr>
2020-08-04 07:31:47 +0000

    tune hyperparams with optuna

--- tune_tabular_regression.py
+++ tune_tabular_regression.py
@@ -7,6 +7,8 @@ from torch.optim import SGD
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.utils.data import DataLoader
 
+import optuna
+
 from src.LT_models import LTLinearRegressor
 from src.monitors import MonitorTree
 from src.optimization import train_stochastic, evaluate
@@ -14,10 +16,10 @@ from src.tabular_datasets import Dataset
 from src.utils import make_directory, TorchDataset
 
 SEED = 1337
-DATA_NAME = "YEAR"
+DATA_NAME = "MICROSOFT"
 LR = 0.001
 BATCH_SIZE = 512 
-EPOCHS = 100
+EPOCHS = 20
 
 # load dataset with same configuration as in https://github.com/Qwicen/node/blob/master/notebooks/year_node_shallow.ipynb
 data = Dataset(DATA_NAME, random_state=SEED, quantile_transform=True, quantile_noise=1e-3, normalize=True)
@@ -34,51 +36,74 @@ print("mean = %.5f, std = %.5f" % (mu, std))
 trainloader = DataLoader(TorchDataset(data.X_train, data.y_train), batch_size=BATCH_SIZE, num_workers=12, shuffle=True)
 valloader = DataLoader(TorchDataset(data.X_valid, data.y_valid), batch_size=BATCH_SIZE*2, num_workers=12, shuffle=False)
 
-for TREE_DEPTH in [6, 8]:
-    for REG in [0] + [10**i for i in range(-3, 4)]:
-        pruning = REG > 0
-        save_dir = Path("./results/tab-datasets-normalized/") / DATA_NAME / "depth={}/reg={}/seed={}".format(TREE_DEPTH, REG, SEED)
-        make_directory(save_dir)
-        model = LTLinearRegressor(TREE_DEPTH, in_features, out_features, pruned=pruning)
+def objective(trial):
 
-        # init optimizer
-        optimizer = SGD(model.parameters(), lr=LR)
+    TREE_DEPTH = trial.suggest_int('TREE_DEPTH', 1, 12)
+    REG = trial.suggest_uniform('REG', 0, 1e5)
 
-        # init learning rate scheduler
-        lr_scheduler = ReduceLROnPlateau(optimizer, 'min', factor=0.1, patience=2)
+    pruning = REG > 0
+    save_dir = Path("./results/optuna/") / DATA_NAME / "depth={}/reg={}/seed={}".format(TREE_DEPTH, REG, SEED)
+    make_directory(save_dir)
+    model = LTLinearRegressor(TREE_DEPTH, in_features, out_features, pruned=pruning)
 
-        # init loss
-        criterion = MSELoss(reduction="sum")
+    # init optimizer
+    optimizer = SGD(model.parameters(), lr=LR)
 
-        # init train-eval monitoring 
-        monitor = MonitorTree(pruning, save_dir)
+    # init learning rate scheduler
+    lr_scheduler = ReduceLROnPlateau(optimizer, 'min', factor=0.1, patience=2)
 
-        state = {
-            'batch-size': BATCH_SIZE,
-            'regression': 'linear',
-            'loss-function': 'MSE',
-            'learning-rate': LR,
-            'seed': SEED,
-            'tree-depth': TREE_DEPTH,
-            'dataset': DATA_NAME,
-            'reg': REG,
-        }
+    # init loss
+    criterion = MSELoss(reduction="sum")
 
-        best_val_loss = float("inf")
-        best_e = -1
-        for e in range(EPOCHS):
-            train_stochastic(trainloader, model, optimizer, criterion, epoch=e, reg=REG, monitor=monitor)
+    # init train-eval monitoring 
+    monitor = MonitorTree(pruning, save_dir)
 
-            val_loss = evaluate(valloader, model, criterion, epoch=e, monitor=monitor)
-            print("Epoch %i: validation loss = %f\n" % (e, val_loss))
+    state = {
+        'batch-size': BATCH_SIZE,
+        'regression': 'linear',
+        'loss-function': 'MSE',
+        'learning-rate': LR,
+        'seed': SEED,
+        'tree-depth': TREE_DEPTH,
+        'dataset': DATA_NAME,
+        'reg': REG,
+    }
 
-            if val_loss <= best_val_loss:
-                best_val_loss = min(val_loss, best_val_loss)
-                best_e = e
-                # save_model(model, optimizer, state, save_dir)
-            
-            # reduce learning rate if needed
-            lr_scheduler.step(val_loss)
+    best_val_loss = float("inf")
+    best_e = -1
+    for e in range(EPOCHS):
+        train_stochastic(trainloader, model, optimizer, criterion, epoch=e, reg=REG, monitor=monitor)
 
-        monitor.close()
-        print("DEPTH={}, REG={}, best validation loss (epoch {}): {}, unscaled {}\n".format(TREE_DEPTH, REG, best_e, best_val_loss, best_val_loss * std ** 2))
+        val_loss = evaluate(valloader, model, criterion, epoch=e, monitor=monitor)
+        print("Epoch %i: validation loss = %f\n" % (e, val_loss))
+
+        if val_loss <= best_val_loss:
+            best_val_loss = min(val_loss, best_val_loss)
+            best_e = e
+            # save_model(model, optimizer, state, save_dir)
+        
+        # reduce learning rate if needed
+        lr_scheduler.step(val_loss)
+
+        trial.report(val_loss * std**2, e)
+        # Handle pruning based on the intermediate value.
+        if trial.should_prune():
+            monitor.close()
+            raise optuna.TrialPruned()
+
+    monitor.close()
+    print("DEPTH={}, REG={}, best validation loss (epoch {}): {}, unscaled {}\n".format(TREE_DEPTH, REG, best_e, best_val_loss, best_val_loss * std ** 2))
+
+    return best_val_loss * std**2
+
+if __name__ == "__main__":
+
+    # Set up the median stopping rule as the pruning condition.
+    study = optuna.create_study(study_name=DATA_NAME, pruner=optuna.pruners.MedianPruner())
+    study.optimize(objective, n_trials=100)
+
+    print(study.best_params, study.best_value)
+    df = study.trials_dataframe(attrs=('number', 'value', 'params', 'state'))
+
+    print(df)
+
