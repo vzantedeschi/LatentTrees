9de84ca57e03e6be6ad2be496d77de91486ef16b
Valentina Zantedeschi <valentina.zantedeschi@univ-st-etienne.fr>
2020-08-28 18:19:32 +0200

    test over 4 different seeds

--- tabular_regression.py
+++ tabular_regression.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 from torch.nn import MSELoss
 from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 from qhoptim.pyt import QHAdam
 
@@ -13,7 +14,6 @@ from src.optimization import train_stochastic, evaluate
 from src.tabular_datasets import Dataset
 from src.utils import make_directory, TorchDataset
 
-SEED = 1225
 DATA_NAME = "YEAR"
 TREE_DEPTH = 3
 REG = 561.7353202746074
@@ -25,71 +25,82 @@ LR = 0.01
 BATCH_SIZE = 512 
 EPOCHS = 100
 
-save_dir = Path("./results/tabular/") / DATA_NAME / "depth={}/reg={}/mlp-layers={}/dropout={}/seed={}".format(TREE_DEPTH, REG, MLP_LAYERS, DROPOUT, SEED)
-make_directory(save_dir)
+test_losses = []
+for SEED in [1225, 1337, 2020, 6021991]:
+    save_dir = Path("./results/tabular/") / DATA_NAME / "depth={}/reg={}/mlp-layers={}/dropout={}/seed={}".format(TREE_DEPTH, REG, MLP_LAYERS, DROPOUT, SEED)
+    make_directory(save_dir)
 
-pruning = REG > 0
+    pruning = REG > 0
 
-data = Dataset(DATA_NAME, random_state=SEED, normalize=True)
-in_features = data.X_train.shape[1]
-out_features = 1
+    data = Dataset(DATA_NAME, random_state=SEED, normalize=True)
+    in_features = data.X_train.shape[1]
+    out_features = 1
 
-# normalize after applying other transformations
-mu, std = data.y_train.mean(), data.y_train.std()
-normalize = lambda x: ((x - mu) / std).astype(np.float32)
-data.y_train, data.y_valid, data.y_test = map(normalize, [data.y_train, data.y_valid, data.y_test])
+    # normalize after applying other transformations
+    mu, std = data.y_train.mean(), data.y_train.std()
+    normalize = lambda x: ((x - mu) / std).astype(np.float32)
+    data.y_train, data.y_valid, data.y_test = map(normalize, [data.y_train, data.y_valid, data.y_test])
 
-print("mean = %.5f, std = %.5f" % (mu, std))
+    print("mean = %.5f, std = %.5f" % (mu, std))
 
-trainloader = DataLoader(TorchDataset(data.X_train, data.y_train), batch_size=BATCH_SIZE, shuffle=True)
-valloader = DataLoader(TorchDataset(data.X_valid, data.y_valid), batch_size=BATCH_SIZE*2, shuffle=False)
-testloader = DataLoader(TorchDataset(data.X_test, data.y_test), batch_size=BATCH_SIZE*2, shuffle=False)
+    trainloader = DataLoader(TorchDataset(data.X_train, data.y_train), batch_size=BATCH_SIZE, shuffle=True)
+    valloader = DataLoader(TorchDataset(data.X_valid, data.y_valid), batch_size=BATCH_SIZE*2, shuffle=False)
+    testloader = DataLoader(TorchDataset(data.X_test, data.y_test), batch_size=BATCH_SIZE*2, shuffle=False)
 
-model = LTRegressor(TREE_DEPTH, in_features, out_features, pruned=pruning, linear=LINEAR, layers=MLP_LAYERS, dropout=DROPOUT)
+    model = LTRegressor(TREE_DEPTH, in_features, out_features, pruned=pruning, linear=LINEAR, layers=MLP_LAYERS, dropout=DROPOUT)
 
-# init optimizer
-optimizer = QHAdam(model.parameters(), lr=LR, nus=(0.7, 1.0), betas=(0.995, 0.998))
+    # init optimizer
+    optimizer = QHAdam(model.parameters(), lr=LR, nus=(0.7, 1.0), betas=(0.995, 0.998))
 
-# init loss
-criterion = MSELoss(reduction="sum")
+    # init loss
+    criterion = MSELoss(reduction="sum")
 
-# init train-eval monitoring 
-monitor = MonitorTree(pruning, save_dir)
+    # init learning rate scheduler
+    lr_scheduler = ReduceLROnPlateau(optimizer, 'min', factor=0.1, patience=2)
 
-state = {
-    'batch-size': BATCH_SIZE,
-    'regression': 'linear',
-    'loss-function': 'MSE',
-    'learning-rate': LR,
-    'seed': SEED,
-    'bst_depth': TREE_DEPTH,
-    'in_size': in_features,
-    'out_size': out_features,
-    'dataset': DATA_NAME,
-    'reg': REG,
-    'pruned': pruning,
-    'linear': LINEAR,
-    'layers': MLP_LAYERS,
-    'dropout': DROPOUT,
-}
+    # init train-eval monitoring 
+    monitor = MonitorTree(pruning, save_dir)
 
-best_val_loss = float("inf")
-best_e = -1
-for e in range(EPOCHS):
-    train_stochastic(trainloader, model, optimizer, criterion, epoch=e, reg=REG, monitor=monitor)
+    state = {
+        'batch-size': BATCH_SIZE,
+        'regression': 'linear',
+        'loss-function': 'MSE',
+        'learning-rate': LR,
+        'seed': SEED,
+        'bst_depth': TREE_DEPTH,
+        'in_size': in_features,
+        'out_size': out_features,
+        'dataset': DATA_NAME,
+        'reg': REG,
+        'pruned': pruning,
+        'linear': LINEAR,
+        'layers': MLP_LAYERS,
+        'dropout': DROPOUT,
+    }
 
-    val_loss = evaluate(valloader, model, {'valid_MSE': criterion}, epoch=e, monitor=monitor)
-    print(f"Epoch {e}: {val_loss}\n")
+    best_val_loss = float("inf")
+    best_e = -1
+    for e in range(EPOCHS):
+        train_stochastic(trainloader, model, optimizer, criterion, epoch=e, reg=REG, monitor=monitor)
 
-    if val_loss['valid_MSE'] <= best_val_loss:
-        best_val_loss = val_loss['valid_MSE']
-        best_e = e
-        LTRegressor.save_model(model, optimizer, state, save_dir, epoch=e, **val_loss)
+        val_loss = evaluate(valloader, model, {'valid_MSE': criterion}, epoch=e, monitor=monitor)
+        print(f"Epoch {e}: {val_loss}\n")
 
-monitor.close()
-print("best validation loss (epoch {}): {}\n".format(best_e, best_val_loss * std ** 2))
+        if val_loss['valid_MSE'] <= best_val_loss:
+            best_val_loss = val_loss['valid_MSE']
+            best_e = e
+            LTRegressor.save_model(model, optimizer, state, save_dir, epoch=e, **val_loss)
 
-model = LTRegressor.load_model(save_dir)
-test_loss = evaluate(testloader, model, {'test_MSE': criterion})
-print("test loss (model of epoch {}): {}\n".format(best_e, test_loss['test_MSE'] * std ** 2))
+        # reduce learning rate if needed
+        lr_scheduler.step(val_loss['valid_MSE'])
 
+    monitor.close()
+    print("best validation loss (epoch {}): {}\n".format(best_e, best_val_loss * std ** 2))
+
+    model = LTRegressor.load_model(save_dir)
+    test_loss = evaluate(testloader, model, {'test_MSE': criterion})
+    print("test loss (model of epoch {}): {}\n".format(best_e, test_loss['test_MSE'] * std ** 2))
+
+    test_losses.append(test_loss['test_MSE'] * std ** 2)
+
+print(np.mean(test_losses), np.std(test_losses))
