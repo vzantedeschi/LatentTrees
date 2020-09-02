2d5f144e8c0ac33ebd638ee871bf6e97fd578c11
Valentina Zantedeschi <valentina.zantedeschi@univ-st-etienne.fr>
2020-09-02 14:04:47 +0000

    bring back support for GLASS, train stochastically

--- hierarchical_clustering.py
+++ hierarchical_clustering.py
@@ -3,6 +3,7 @@ import numpy as np
 from pathlib import Path
 from tqdm import tqdm
 
+import torch
 from torch.nn import MSELoss
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.utils.data import DataLoader
@@ -16,10 +17,9 @@ from src.optimization import train_stochastic, evaluate
 from src.tabular_datasets import Dataset
 from src.utils import make_directory, TorchDataset
 
-SEED = 1337
-DATA_NAME = "ALOI"
-TREE_DEPTH = 2
-REG = 0
+DATA_NAME = "GLASS"
+TREE_DEPTH = 8
+REG = 498.073
 LR = 0.1
 EPOCHS = 100
 
@@ -35,93 +35,106 @@ else:
 
 print("Training on", device)
 
-data = Dataset(DATA_NAME, random_state=SEED, normalize=True)
-classes = np.unique(data.y_train)
-num_classes = max(classes) + 1
-
 # selecting input and output features for self-supervised training
 if DATA_NAME == "ALOI":
     in_features = [0, 2] # R and B
     out_features = [1] # G
     BATCH_SIZE = 128
+    SPLIT = 'conv'
 
 elif DATA_NAME == "COVTYPE":
     out_features = [3, 4]
     in_features = list(set(range(54)) - set(out_features))
     BATCH_SIZE = 512
+    SPLIT = 'linear'
 
-save_dir = Path("./results/clustering-selfsup/") / DATA_NAME / "out-feats={}/depth={}/reg={}/seed={}".format(out_features, TREE_DEPTH, REG, SEED)
+elif DATA_NAME == "GLASS":
+    out_features = [7, 8]
+    in_features = list(set(range(9)) - set(out_features))
+    BATCH_SIZE = 8
+    SPLIT = 'linear'
 
-save_dir.mkdir(parents=True, exist_ok=True)
+test_scores= []
+for SEED in [1225, 1337, 2020, 6021991]:
 
-if DATA_NAME == "ALOI":
-    trainloader = DataLoader(TorchDataset(data.X_train_in, data.X_train_out, means=(data.mean[in_features], data.mean[out_features]), stds=(data.std[in_features], data.std[out_features])), batch_size=BATCH_SIZE, shuffle=True, num_workers=12, pin_memory=pin_memory)
-    valloader = DataLoader(TorchDataset(data.X_valid_in, data.X_valid_out, means=(data.mean[in_features], data.mean[out_features]), stds=(data.std[in_features], data.std[out_features])), batch_size=BATCH_SIZE*2, shuffle=False, num_workers=12, pin_memory=pin_memory)
-    testloader = DataLoader(TorchDataset(data.X_test_in, data.X_test_out, means=(data.mean[in_features], data.mean[out_features]), stds=(data.std[in_features], data.std[out_features])), batch_size=BATCH_SIZE*2, shuffle=False, num_workers=12, pin_memory=pin_memory)
+    data = Dataset(DATA_NAME, random_state=SEED, normalize=True)
+    classes = np.unique(data.y_train)
+    num_classes = max(classes) + 1
 
-else:
-    trainloader = DataLoader(TorchDataset(data.X_train_in, data.X_train_out), batch_size=BATCH_SIZE, shuffle=True, num_workers=12, pin_memory=pin_memory)
-    valloader = DataLoader(TorchDataset(data.X_valid_in, data.X_valid_out), batch_size=BATCH_SIZE*2, shuffle=False, num_workers=12, pin_memory=pin_memory)
-    testloader = DataLoader(TorchDataset(data.X_test_in, data.X_test_out), batch_size=BATCH_SIZE*2, shuffle=False, num_workers=12, pin_memory=pin_memory)
+    save_dir = Path("./results/clustering-selfsup/") / DATA_NAME / "out-feats={}/depth={}/reg={}/seed={}".format(out_features, TREE_DEPTH, REG, SEED)
 
-model = LTRegressor(TREE_DEPTH, data.X_train_in.shape[1:], data.X_train_out.shape[1:], pruned=pruning, split_func='conv')
+    save_dir.mkdir(parents=True, exist_ok=True)
 
-print(model.count_parameters(), "model's parameters")
-# init optimizer
-optimizer = QHAdam(model.parameters(), lr=LR, nus=(0.7, 1.0), betas=(0.995, 0.998))
+    data.X_train_in, data.X_valid_in, data.X_test_in = data.X_train[:, in_features], data.X_valid[:, in_features], data.X_test[:, in_features]
+    data.X_train_out, data.X_valid_out, data.X_test_out = data.X_train[:, out_features], data.X_valid[:, out_features], data.X_test[:, out_features]
+    
+    if DATA_NAME == "ALOI":
+        trainloader = DataLoader(TorchDataset(data.X_train_in, data.X_train_out, means=(data.mean[in_features], data.mean[out_features]), stds=(data.std[in_features], data.std[out_features])), batch_size=BATCH_SIZE, shuffle=True, num_workers=12, pin_memory=pin_memory)
+        valloader = DataLoader(TorchDataset(data.X_valid_in, data.X_valid_out, means=(data.mean[in_features], data.mean[out_features]), stds=(data.std[in_features], data.std[out_features])), batch_size=BATCH_SIZE*2, shuffle=False, num_workers=12, pin_memory=pin_memory)
+        testloader = DataLoader(TorchDataset(data.X_test_in, data.X_test_out, means=(data.mean[in_features], data.mean[out_features]), stds=(data.std[in_features], data.std[out_features])), batch_size=BATCH_SIZE*2, shuffle=False, num_workers=12, pin_memory=pin_memory)
 
-# init learning rate scheduler
-lr_scheduler = ReduceLROnPlateau(optimizer, 'min', factor=0.1, patience=2)
+    else:
+        trainloader = DataLoader(TorchDataset(data.X_train_in, data.X_train_out), batch_size=BATCH_SIZE, shuffle=True, num_workers=12, pin_memory=pin_memory)
+        valloader = DataLoader(TorchDataset(data.X_valid_in, data.X_valid_out), batch_size=BATCH_SIZE*2, shuffle=False, num_workers=12, pin_memory=pin_memory)
+        testloader = DataLoader(TorchDataset(data.X_test_in, data.X_test_out), batch_size=BATCH_SIZE*2, shuffle=False, num_workers=12, pin_memory=pin_memory)
 
-# init loss
-criterion = MSELoss(reduction="sum")
+    model = LTRegressor(TREE_DEPTH, data.X_train_in.shape[1:], data.X_train_out.shape[1:], pruned=pruning, split_func=SPLIT)
 
-# init train-eval monitoring 
-monitor = MonitorTree(pruning, save_dir)
+    print(model.count_parameters(), "model's parameters")
+    # init optimizer
+    optimizer = QHAdam(model.parameters(), lr=LR, nus=(0.7, 1.0), betas=(0.995, 0.998))
 
-state = {
-    'batch-size': BATCH_SIZE,
-    'loss-function': 'MSE',
-    'learning-rate': LR,
-    'seed': SEED,
-    'dataset': DATA_NAME,
-    'reg': REG,
-    'linear': True,
-}
+    # init learning rate scheduler
+    lr_scheduler = ReduceLROnPlateau(optimizer, 'min', factor=0.1, patience=2)
 
-best_val_loss = float('inf')
-best_e = -1
-no_improv = 0
-for e in range(EPOCHS):
-    train_stochastic(trainloader, model, optimizer, criterion, epoch=e, reg=REG, monitor=monitor)
+    # init loss
+    criterion = MSELoss(reduction="sum")
 
-    val_loss = evaluate(valloader, model, {'MSE': criterion}, epoch=e, monitor=monitor)
+    # init train-eval monitoring 
+    monitor = MonitorTree(pruning, save_dir)
 
-    print("Epoch %i: validation mse = %f\n" % (e, val_loss['MSE']))
+    state = {
+        'batch-size': BATCH_SIZE,
+        'loss-function': 'MSE',
+        'learning-rate': LR,
+        'seed': SEED,
+        'dataset': DATA_NAME,
+        'reg': REG,
+        'linear': True,
+    }
 
-    monitor.write(model, e, val={"Dendrogram Purity": score})
+    best_val_loss = float('inf')
+    best_e = -1
+    no_improv = 0
+    for e in range(EPOCHS):
+        train_stochastic(trainloader, model, optimizer, criterion, epoch=e, reg=REG, monitor=monitor)
 
-    no_improv += 1
-    if val_loss['MSE'] <= best_val_loss:
-        best_val_loss = val_loss['MSE']
-        best_e = e
-        LTRegressor.save_model(model, optimizer, state, save_dir, epoch=e, val_mse=val_loss['MSE'])
-        no_improv = 0
+        val_loss = evaluate(valloader, model, {'MSE': criterion}, epoch=e, monitor=monitor)
 
-    # reduce learning rate if needed
-    lr_scheduler.step(val_loss['MSE'])
-    monitor.write(model, e, train={"lr": optimizer.param_groups[0]['lr']})
+        print("Epoch %i: validation mse = %f\n" % (e, val_loss['MSE']))
 
-    if no_improv == 10:
-        break
+        no_improv += 1
+        if val_loss['MSE'] <= best_val_loss:
+            best_val_loss = val_loss['MSE']
+            best_e = e
+            LTRegressor.save_model(model, optimizer, state, save_dir, epoch=e, val_mse=val_loss['MSE'])
+            no_improv = 0
 
-monitor.close()
+        # reduce learning rate if needed
+        lr_scheduler.step(val_loss['MSE'])
+        monitor.write(model, e, train={"lr": optimizer.param_groups[0]['lr']})
 
-model = LTRegressor.load_model(save_dir)
+        if no_improv == EPOCHS // 5:
+            break
 
-score, _ = LT_dendrogram_purity(data.X_valid_in, data.y_valid, model, model.latent_tree.bst, num_classes)
+    monitor.close()
 
-print("Epoch %i: validation purity = {}\n".format(best_e, score))
+    model = LTRegressor.load_model(save_dir)
 
-score, _ = LT_dendrogram_purity(data.X_test_in, data.y_test, model, model.latent_tree.bst, num_classes)
-print("Epoch %i: test mse = %f; test purity = %f\n" % (best_e, test_loss['MSE'], score))
+    score, _ = LT_dendrogram_purity(data.X_test_in, data.y_test, model, model.latent_tree.bst, num_classes)
+    print("Epoch %i: test purity = %f\n" % (best_e, score))
+    
+    test_scores.append(score)
+
+print(np.mean(test_scores), np.std(test_scores))
+np.save(save_dir / '../test-scores.npy', test_scores)
