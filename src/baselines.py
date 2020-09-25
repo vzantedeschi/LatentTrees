0cec33a1180177c5db6a1420b049777d9fa52a8b
Valentina Zantedeschi <valentina.zantedeschi@univ-st-etienne.fr>
2020-09-25 14:11:33 +0200

    benchmark dntn

--- src/baselines.py
+++ src/baselines.py
@@ -1,5 +1,8 @@
 import numpy as np
 import pandas as pd
+import torch
+
+from functools import reduce
 
 from pyoptree.optree import OptimalTreeModel
 
@@ -60,3 +63,82 @@ class OptTree():
             z[:, self.bst.desc_right] = np.minimum(z[:, self.bst.desc_right], z[:, self.bst.split_nodes])
 
         return z
+
+""" Code adapted from https://github.com/wOOL/DNDT/blob/master/pytorch/demo.ipynb"""
+
+def torch_kron_prod(a, b):
+    res = torch.einsum('ij,ik->ijk', [a, b])
+    res = torch.reshape(res, [-1, np.prod(res.shape[1:])])
+    return res
+
+def torch_bin(x, cut_points, temperature=0.1):
+    # x is a N-by-1 matrix (column vector)
+    # cut_points is a D-dim vector (D is the number of cut-points)
+    # this function produces a N-by-(D+1) matrix, each row has only one element being one and the rest are all zeros
+    D = cut_points.shape[0]
+    W = torch.reshape(torch.linspace(1.0, D + 1.0, D + 1), [1, -1])
+    cut_points, _ = torch.sort(cut_points)  # make sure cut_points is monotonically increasing
+    b = torch.cumsum(torch.cat([torch.zeros([1]), -cut_points], 0),0)
+    h = torch.matmul(x, W) + b
+    res = torch.exp(h-torch.max(h))
+    res = res/torch.sum(res, dim=-1, keepdim=True)
+    return h
+
+class DNDT(torch.nn.Module):
+
+    def __init__(self, in_size, num_classes, num_cuts=1, temperature=0.1):
+
+        super().__init__()
+
+        self.params = locals()
+        del self.params['self']
+
+        self.cuts = [num_cuts] * in_size
+        self.num_leaves = np.prod(np.array(self.cuts) + 1)
+        self.num_classes = num_classes
+        self.temperature = temperature
+
+        self.cut_points_list = [torch.nn.Parameter(torch.rand([i], requires_grad=True)) for i in self.cuts]
+        self.leaf_score = torch.nn.Parameter(torch.rand([self.num_leaves, num_classes], requires_grad=True))
+
+    def forward(self, x):
+
+        leaf = reduce(torch_kron_prod,
+                  map(lambda z: torch_bin(x[:, z[0]:z[0] + 1], z[1], self.temperature), enumerate(self.cut_points_list)))
+        
+        return torch.matmul(leaf, self.leaf_score)
+
+    def count_parameters(self):
+        return sum(p.numel() for p in self.parameters() if p.requires_grad)
+
+    @classmethod
+    def load_model(cls, load_dir, add_load=None):
+        
+        checkpoint = torch.load(Path(load_dir) / 'model.t7', map_location=torch.device('cpu'))
+        
+        model = cls(**checkpoint, **checkpoint['kwargs'])
+        model.load_state_dict(checkpoint['model_state_dict'])
+
+        if add_load is not None:
+            if 'optimizer' in add_load.keys():
+                add_load['optimizer'].load_state_dict(checkpoint['optimizer_state_dict'])
+
+            if 'checkpoint' in add_load.keys():
+                add_load['checkpoint'] = checkpoint
+        
+        return model
+  
+    def save_model(self, optimizer, state, save_dir, **kwargs):
+
+        try:
+            state_dict = self.module.state_dict()
+
+        except AttributeError:
+            state_dict = self.state_dict()
+
+        state['model_state_dict'] = state_dict
+        state['optimizer_state_dict'] = optimizer.state_dict()
+        state.update(self.params)
+        state.update(kwargs)
+
+        torch.save(state, Path(save_dir) / 'model.t7')
