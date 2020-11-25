f4fbe99227dabb030c4206b44c203f5499c58193
Valentina Zantedeschi <valentina.zantedeschi@univ-st-etienne.fr>
2020-11-25 14:47:16 +0100

    solve minors

--- supervised_tabular.py
+++ supervised_tabular.py
@@ -4,13 +4,13 @@ import numpy as np
 
 from pathlib import Path
 
-from torch.nn import BCELoss
+from torch.nn import BCELoss, MSELoss
 from torch.utils.data import DataLoader
 
 from qhoptim.pyt import QHAdam
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 
-from src.LT_models import LTBinaryClassifier
+from src.LT_models import LTBinaryClassifier, LTRegressor
 from src.monitors import MonitorTree
 from src.optimization import train_stochastic, evaluate
 from src.datasets import Dataset, TorchDataset
@@ -18,12 +18,14 @@ from src.utils import deterministic
 
 import time
 
+clf_datasets = ["CLICK", "HIGGS", "MUSHROOMS"]
+
 @hydra.main(config_path='config/tuned-click.yaml')
 def main(cfg):
 
     cwd = Path(hydra.utils.get_original_cwd())
 
-    if cfg.DATA_NAME in ["CLICK", "HIGGS"]:
+    if cfg.DATA_NAME in clf_datasets:
         data = Dataset(cfg.DATA_NAME, data_path=cwd / "DATA", normalize=True, seed=459107)
         print('classes', np.unique(data.y_test))
 
@@ -52,11 +54,11 @@ def main(cfg):
 
         print("results will be saved in:", save_dir.resolve())
 
-        trainloader = DataLoader(TorchDataset(data.X_train, data.y_train), batch_size=BATCH_SIZE, shuffle=True)
-        valloader = DataLoader(TorchDataset(data.X_valid, data.y_valid), batch_size=BATCH_SIZE*2, shuffle=False)
-        testloader = DataLoader(TorchDataset(data.X_test, data.y_test), batch_size=BATCH_SIZE*2, shuffle=False)
+        trainloader = DataLoader(TorchDataset(data.X_train, data.y_train), batch_size=cfg.training.BATCH_SIZE, num_workers=cfg.training.WORKERS, shuffle=True)
+        valloader = DataLoader(TorchDataset(data.X_valid, data.y_valid),  num_workers=cfg.training.WORKERS, batch_size=cfg.training.BATCH_SIZE*2, shuffle=False)
+        testloader = DataLoader(TorchDataset(data.X_test, data.y_test),  num_workers=cfg.training.WORKERS, batch_size=cfg.training.BATCH_SIZE*2, shuffle=False)
 
-        if cfg.DATA_NAME in ["CLICK", "HIGGS"]:
+        if cfg.DATA_NAME in clf_datasets:
 
             model_cls = LTBinaryClassifier
             model = LTBinaryClassifier(cfg.model.TREE_DEPTH, data.X_train.shape[1], pruned=pruning, linear=cfg.model.LINEAR, layers=cfg.model.MLP_LAYERS, dropout=cfg.model.DROPOUT)
@@ -74,11 +76,11 @@ def main(cfg):
 
             # init loss
             criterion = MSELoss(reduction="sum")
-            eval_criterion = criterion
+            eval_criterion = lambda x, y: criterion(x, y) * data.std_y**2
             state['loss-function'] = 'MSE'
 
         # init optimizer
-        optimizer = QHAdam(model.parameters(), lr=LR, nus=(0.7, 1.0), betas=(0.995, 0.998))
+        optimizer = QHAdam(model.parameters(), lr=cfg.training.LR, nus=(0.7, 1.0), betas=(0.995, 0.998))
 
         # init learning rate scheduler
         lr_scheduler = ReduceLROnPlateau(optimizer, 'min', factor=0.1, patience=2)
@@ -103,7 +105,7 @@ def main(cfg):
                 no_improv = 0
                 model_cls.save_model(model, optimizer, state, save_dir, epoch=e, val_loss=best_val_loss)
 
-            if no_improv == EPOCHS // 5:
+            if no_improv == cfg.training.EPOCHS // 10:
                 break
 
             # reduce learning rate if needed
