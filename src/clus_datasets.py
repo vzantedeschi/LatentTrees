31484a70f47eec80737fccf29db85e2f94ae4b5e
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-17 11:43:43 +0000

    Adding tarfile member sanitization to extractall()

--- src/clus_datasets.py
+++ src/clus_datasets.py
@@ -81,7 +81,29 @@ def fetch_ALOI(path, valid_size=0.2, test_size=0.2, seed=None):
 
             with tarfile.open(archive_path, 'r') as f_in:
 
-                f_in.extractall(path=data_path)
+                
+                import os
+                
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(f_in, path=data_path)
 
     if not npz_path.exists():
         
